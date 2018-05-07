5961f1f36a0f907b9028b049270ae302b65e18c0
Konstantinos <k.lampridis@hotmail.com>
2018-05-07 22:00:02 +0200

    Pass 3rd unittest: test strain coordinates request

--- green_web/tests/test_server.py
+++ green_web/tests/test_server.py
@@ -1,17 +1,15 @@
-import requests
 import os
 import sys
 import json
 import unittest
 import tempfile
+import requests
+from random import randint
+
 from green_web import green_app
 
-d = dict(columns=20,
-         grid='rectangular',
-         initialization='pca',
-         rows=10,
-         type='toroid'
-    )
+# Random order for tests runs. (Original is: -1 if x<y, 0 if x==y, 1 if x>y).
+unittest.TestLoader.sortTestMethodsUsing = lambda _, x, y: randint(-1, 1)
 
 
 class FlaskrTestCase(unittest.TestCase):
@@ -20,19 +18,27 @@ class FlaskrTestCase(unittest.TestCase):
         # self.db_fd, green_app.config['DATABASE'] = tempfile.mkstemp()
         green_app.testing = True
         self.app = green_app.test_client()
-        self.strain_id = 'amnesia'
-        self.map_specs = {
+        self.strain_id1 = 'amnesia'
+        self.strain_id2 = 'silver-haze'
+        self.map_specs1 = {
             'columns': 5,
-            'grid': 'rectangular',
+            'grid': 'hexagonal',
             'initialization': 'pca',
             'rows': 7,
             'type': 'toroid'
         }
-        self.ms0 = {'rows': 10}
-        self.ms = d
-        self.map_specs2 = b'{"columns": 20, "grid": "rectangular", "initialization": "pca", "rows": 10, "type": "toroid"}'
+        self.map_specs2 = {
+            'columns': 4,
+            'grid': 'rectangular',
+            'initialization': 'random',
+            'rows': 8,
+            'type': 'planar'
+        }
         # TODO decouple the 'new-dt' component of the id
-        self.map_id = 'somoclu_new-dt_pca_toroid_rectangular_7_5'
+        self.map_id1 = 'somoclu_new-dt_pca_toroid_hexagonal_7_5'
+        self.map_id2 = 'somoclu_new-dt_random_planar_rectangular_8_4'
+        _ = self.app.post('/api/strain/map', data=json.dumps(self.map_specs1), headers={"Content-Type": "application/json"})
+        # data = json.loads(response.get_data(as_text=True))
         # self.map_id2 = "somoclu_new-dt_pca_toroid_rectangular_20_10"
         # with green_app.app_context():
         #     flaskr.init_db()
@@ -43,7 +49,7 @@ class FlaskrTestCase(unittest.TestCase):
         # os.unlink(flaskr.app.config['DATABASE'])
 
     def test_strain_id_endpoint(self):
-        response = self.app.get('/api/strain/'+self.strain_id)
+        response = self.app.get('/api/strain/'+self.strain_id1)
         data = json.loads(response.get_data(as_text=True))
         self.assertIn('flavors', data)
         self.assertIn('name', data)
@@ -53,16 +59,28 @@ class FlaskrTestCase(unittest.TestCase):
         self.assertEqual(data['type'], 'sativa')
 
     def test_map_creation_endpoint(self):
-        # dictToSend = self.map_specs
-        # res = requests.post('http://localhost:5555/api/strain/map', json=self.map_specs)
-        # print('response from server:', res.text)
-        # dictFromServer = res.json()
-        # mpeta = json.dumps(self.map_specs)
-        response = self.app.post('/api/strain/map', data=json.dumps(self.map_specs), headers={"Content-Type": "application/json"})
+        response = self.app.post('/api/strain/map', data=json.dumps(self.map_specs2), headers={"Content-Type": "application/json"})
         data = json.loads(response.get_data(as_text=True))
         self.assertIn('map_id', data)
-        self.assertEqual(data['map_id'], self.map_id)
+        self.assertEqual(data['map_id'], self.map_id2)
+
+    def test_strain_coordinates_request(self):
+        response = self.app.post('/api/strain/' + self.strain_id2)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(float(data['x']), int(data['x']))
+        self.assertEqual(float(data['y']), int(data['y']))
+        self.assertGreaterEqual(int(data['x']), 0)
+        self.assertGreaterEqual(int(data['y']), 0)
+        self.assertLessEqual(int(data['x']), int(data['map_specs']['columns']))
+        self.assertLessEqual(int(data['y']), int(data['map_specs']['rows']))
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=0)
+
+
+    # dictToSend = self.map_specs
+    # res = requests.post('http://localhost:5555/api/strain/map', json=self.map_specs)
+    # print('response from server:', res.text)
+    # dictFromServer = res.json()
+    # mpeta = json.dumps(self.map_specs)
